4b0032431e08606e44afeaeaa5da1a058f16c1ea
Toruniiak <p.stawickii@gmail.com>
2026-05-05 01:04:00 +0200

    Delete app.js

--- app.js
+++
@@ -1,8 +0,0 @@
-document.querySelectorAll(".btn").forEach(btn => {
-    btn.addEventListener("click", () => {
-        btn.style.transform = "scale(0.95)";
-        setTimeout(() => {
-            btn.style.transform = "scale(1)";
-        }, 150);
-    });
-});
