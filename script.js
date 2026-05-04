330787d0e4f54bb11487a29b81f5cfb836c0ec6a
Toruniiak <p.stawickii@gmail.com>
2026-05-05 01:03:31 +0200

    Update and rename testy.html to script.js

---
+++ script.js
@@ -0,0 +1,12 @@
+function showMemes() {
+  alert("📸 Tu będą memy!\n\nW przyszłości możesz tu dodać galerię memów.");
+}
+
+function startRiddles() {
+  alert("🧠 Zagadka dnia:\n\nCo ma szyję, ale nie ma głowy?");
+  // Tu możesz rozbudować do prawdziwych zagadek
+}
+
+function startTest() {
+  alert("🔥 Test 'Kto jest Twoim ziomkiem?' już wkrótce!");
+}
